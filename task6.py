96c6338ca67efac075c09456c0cc774d26273a19
olenadurytska <90180454+olenadurytska@users.noreply.github.com>
2021-10-18 21:22:00 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,20 +1,26 @@
-a = 0
-b = 0
-x = list(map(int, input()))
-print('The total number of items in the list', len(x))
-print('The last item in the list', x[-1])
-x.reverse()
-print('The list in reverse order', x)
-for i in x:
-    if i == 5:
-        a += 1
-    elif i < 5:
-        b += 1
-if a > 0:
-    print('This list contains a 5,', 'number of 5 : ', a)
-else:
-    print('This list do not contains a 5')
-print(b, 'integers in the list are less than 5')
+x = int(input("Enter number of elements : "))
+l = []
+for i in range(0, x):
+    ll = int(input())
+    l.append(ll) 
+     
+print(l)
 
+print('The total number of items in the list:', len(l))
+last_item = l[-1]
+print('The last item in the list:' , last_item)
+l.reverse()
+print('The list in reverse order:', l)
+if (5 in l) :
+    print("YES")
+else:
+    print("NO")
+    
+print('Number of 5:', l.count(5))
+l.pop(0)
+l.pop()
+l.sort()
+print(l)
+print(len([1 for i in l if i > 5]), 'integers in the list are less than 5')
 
 
