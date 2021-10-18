6751b123e376eb3524c32dce3e148b4de0f129bf
olenadurytska <90180454+olenadurytska@users.noreply.github.com>
2021-10-18 21:43:49 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,3 +1,15 @@
+'''
+Write a function that asks the user to enter a list of integers. Do the following:
+
+Print the total number of items in the list.
+Print the last item in the list.
+Print the list in reverse order.
+Print Yes if the list contains a 5 and No otherwise.
+Print the number of fives in the list.
+Remove the first and last items from the list, sort the remaining items, and print the result.
+Print how many integers in the list are less than 5.
+'''
+
 x = int(input("Enter number of elements : "))
 l = []
 for i in range(0, x):
