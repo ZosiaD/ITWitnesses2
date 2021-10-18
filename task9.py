d2ce08fefc55188623c4f3f952b6f0c4ef20dc04
olenadurytska <90180454+olenadurytska@users.noreply.github.com>
2021-10-18 21:42:40 +0300

    Update task9.py

--- task9.py
+++ task9.py
@@ -1,10 +1,16 @@
-x = input('enter zero: ')
-if x == '0':
-    print("true")
-else:
-    print('false')
-if x.isdigit():
-    print('it is a number')
-else:
-    print('it is not a number')
+'''
+Write a program that asks the user to enter an input.
 
+Print whether it is True or False
+Check if an input is None or NaN and print the result of the check
+'''
+
+import math
+
+inpuut = input('Enter an input:')
+print(bool(inpuut))
+
+if float(inpuut) is None:
+    print('Input is None')
+elif math.isnan(float(inpuut)):
+    print('Input is Nan')
